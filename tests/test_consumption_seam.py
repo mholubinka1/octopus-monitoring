cea83dd64276ca62aff34ed3c51215608b8d4df3
Matt Holubinka <meholubinka@gmail.com>
2026-07-14 21:15:26 +0100

    Add pytest test infrastructure and CI gate (#366)

---
+++ tests/test_consumption_seam.py
@@ -0,0 +1,66 @@
+from datetime import datetime, timezone
+from decimal import Decimal
+
+import responses
+from common.config import OctopusAPISettings
+from data.model import Energy
+from data.mysql import sql_models
+from data.mysql.client import MariaDBClient
+from data.octopus.api import OctopusEnergyAPIClient
+from data.octopus.model import Agreement, Electricity
+
+CONSUMPTION_ENDPOINT = (
+    "https://api.octopus.energy/v1/electricity-meter-points/"
+    "1234567890123/meters/00A1234567/consumption/"
+)
+
+
+@responses.activate
+def test_consumption_fetched_from_octopus_is_persisted_and_queryable(
+    mariadb_client: MariaDBClient,
+) -> None:
+    responses.add(
+        responses.GET,
+        CONSUMPTION_ENDPOINT,
+        json={
+            "results": [
+                {
+                    "consumption": "1.234",
+                    "interval_start": "2026-01-01T00:00:00+00:00",
+                    "interval_end": "2026-01-01T00:30:00+00:00",
+                }
+            ],
+            "next": None,
+        },
+        status=200,
+    )
+
+    octopus = OctopusEnergyAPIClient(
+        OctopusAPISettings(
+            {"octopus": {"account_number": "A-1234ABCD", "api_key": "sk_live_test"}}
+        )
+    )
+    meter = Electricity(
+        mpan="1234567890123",
+        serial_number="00A1234567",
+        agreements=[
+            Agreement(
+                tariff_code="E-1R-VAR-22-11-01-A",
+                valid_from=datetime(2022, 11, 1, tzinfo=timezone.utc),
+                valid_to=None,
+            )
+        ],
+    )
+
+    _, consumption = octopus.get_consumption_directly_from_endpoint(
+        Energy.electricity, CONSUMPTION_ENDPOINT
+    )
+
+    mariadb_client.write_consumption(meter, consumption)
+
+    with mariadb_client.session_read_scope() as session:
+        stored = session.query(sql_models.consumption).all()
+
+    assert len(stored) == 1
+    assert stored[0].id == "E20260101000000"
+    assert Decimal(str(stored[0].est_kwh)) == Decimal("1.234")
